2f2aad066c6f630803e46c79aac2b3b1478aef98
facebesidewyj <wang.yijin@foxmail.com>
2019-01-27 00:27:20 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@
  */
 module.exports = {
   root: true,
+  // 设定解析器
+  parser: 'babel-eslint',
   // 设置js语言的解析配置
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   // 指定开发环境，配置全局变量
   env: {
     browser: true,
     node: true,
-    es6: true
+    es6: true,
+    jest: true
   },
   // 继承规则
   extends: 'standard',
