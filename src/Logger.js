4b3b67a1239eeb5ee065bd8a549563f35c0990a4
facebesidewyj <wang.yijin@foxmail.com>
2019-01-27 16:28:52 +0800

    update

--- src/Logger.js
+++ src/Logger.js
@@ -12,14 +12,14 @@ class Logger {
   constructor({ color, fontSize, hide }) {
     this.color = color || '#333'
     this.fontSize = fontSize || '12px'
-    this.hide = hide || true
+    this.hide = hide || false
   }
   /**
    * 打印log信息
    * @param {Object} value 打印信息对象
    */
   log(value) {
-    if (!this.hide) {
+    if (this.hide) {
       return false
     }
     console.log(`%c${value}`, `color:${this.color}; font-size:${this.fontSize}; text-shadow: 0 0 5px rgba(0,0,0,0.2);`)
